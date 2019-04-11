41a8b1087033774686f245c1c7b320ebd5e38349
Andres <jhonatan.leon@farmatodo.com>
2019-04-11 13:25:25 -0500

    se configura conexion local y en mongo db atlas

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,25 @@
 //  ==============================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//  ==============================
+// Entorno
+//  ==============================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+
+
+//  ==============================
+// Base de datos
+//  ==============================
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://andres:Hack123456@cluster0-6ay8o.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -9,35 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', function(req, res) {
-    res.json('get usuario')
-})
+app.use(require('./routes/usuario'));
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'el nombre es necesario'
-        })
-    } else {
-        res.json({
-            id
-        })
-    }
-})
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
+    if (err) throw err;
 
+    console.log("conectado");
 
+});
 
-
-})
-
-app.delete('/', function(req, res) {
-    res.json('delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log("escuchando el puerto", process.env.PORT);
