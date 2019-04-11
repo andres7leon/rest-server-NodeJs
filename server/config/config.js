09d12ea51b3fa0562e606e0d0b8678e0f53b00c0
Andres <jhonatan.leon@farmatodo.com>
2019-04-11 13:40:00 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://andres:Hack123456@cluster0-6ay8o.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
